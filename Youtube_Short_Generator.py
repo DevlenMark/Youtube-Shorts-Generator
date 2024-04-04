de71b5d3ef51420ab4f95b3f91abf41fa82dd128
DevlenMark <107339764+DevlenMark@users.noreply.github.com>
2024-04-04 01:41:06 -0700

    Update Youtube_Short_Generator.py with Comments

--- Youtube_Short_Generator.py
+++ Youtube_Short_Generator.py
@@ -13,16 +13,20 @@ def videoShortGenerator(search):
 def clipEditor(filename, start_time):
     # Set up YT Video
     yt_video = VideoFileClip(filename)
+    #duration is 60 seconds
     end_time = start_time + 60
+    #Makes sure that the end time is not longer thn the video itself
     if (end_time > yt_video.duration):
         end_time = yt_video.duration
     clip1 = VideoFileClip(filename).subclip(start_time, end_time)
+    #1080 is the width of a phone screen resolution
     clip1_resized = clip1.resize(width=1080)
 
     # Set up Minecraft Parkcore
     mc_playtime = end_time - start_time
     mc_video = VideoFileClip(
-        r'Youtube-Shorts-Generator/YT Videos/13 Minutes Minecraft Parkour Gameplay [Free to Use] [Map Download].mp4')
+        r'Youtube-Shorts-Generator/YT Videos/13 Minutes Minecraft Parkour Gameplay [Free to Use] [Map Download].mp4') #Replace with your file location of downloaded Minecraft Video
+    #Finds random part of the video
     mc_start_time = random.randint(0, int(mc_video.duration - mc_playtime))
     clip2 = mc_video.subclip(mc_start_time, mc_start_time + mc_playtime)
     clip2_resized = clip2.resize(width=1080)
@@ -33,8 +37,10 @@ def clipEditor(filename, start_time):
     #set up background
     background_clip = ColorClip(size=(1080, 1920), color=(0, 0, 0)).set_duration(combined.duration)
 
+    #Put background into the clip
     final_clip = CompositeVideoClip([background_clip, combined.set_position(("center", "center"))])
 
+    #Download clip
     final_clip.write_videofile('Youtube-Shorts-Generator/Final Short/final.mp4')
 
-videoShortGenerator('https://www.youtube.com/watch?v=ozzoeMs9ks0')
+videoShortGenerator('https://www.youtube.com/watch?v=ozzoeMs9ks0')#Replace with link or search you wish for
